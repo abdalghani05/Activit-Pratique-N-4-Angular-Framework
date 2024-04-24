87da46123511383765180db8ed7221d953842f7c
NAIRI <anairi@winxo.com>
2024-04-24 15:15:34 +0100

    Première Partie (First Demo) :

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'FirstApp';
+
+  public currentRoute : any ;
+  constructor(private router : Router){
+
+  }
+  gotohome() {
+    this.currentRoute = "home";
+    this.router.navigateByUrl("/home")
+  }
+gotoproducts() {
+  this.currentRoute = "products";
+  this.router.navigateByUrl("/products")
+}
+
 }
